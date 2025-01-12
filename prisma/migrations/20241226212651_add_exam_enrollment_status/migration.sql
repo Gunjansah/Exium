e63d993a009a20cba7aa26b2e510174a7c630123
Kandel-Bibas <bibas1xy@gmail.com>
2025-01-11 18:23:09 -0600

    DB Reset

--- prisma/migrations/20241226212651_add_exam_enrollment_status/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "ExamEnrollmentStatus" AS ENUM ('NOT_STARTED', 'IN_PROGRESS', 'COMPLETED', 'SUBMITTED');
-
--- AlterTable
-ALTER TABLE "exam_enrollments" ADD COLUMN     "endTime" TIMESTAMP(3),
-ADD COLUMN     "startTime" TIMESTAMP(3),
-ADD COLUMN     "status" "ExamEnrollmentStatus" NOT NULL DEFAULT 'NOT_STARTED';
